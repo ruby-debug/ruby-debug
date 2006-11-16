931a0422f5983f2f5e0315e92db789b1b8399f78
kent <kent@453b2852-3d18-0410-866e-d09c099698e4>
2006-11-16 00:04:04 +0000

    fixed debug_method for assignment methods
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/branches/stable@19 453b2852-3d18-0410-866e-d09c099698e4

--- lib/ruby-debug.rb
+++ lib/ruby-debug.rb
@@ -216,12 +216,14 @@ class Module
   # Wraps the +meth+ method with Debugger.start {...} block.
   #
   def debug_method(meth)
-    alias_method "__debugee_#{meth}".to_sym, meth
+    old_meth = "__debugee_#{meth}"
+    old_meth = "#{$1}_set" if old_meth =~ /^(.+)=$/
+    alias_method old_meth.to_sym, meth
     class_eval <<-EOD
     def #{meth}(*args, &block)
       Debugger.start do
         debugger 2
-        __debugee_#{meth}(*args, &block)
+        #{old_meth}(*args, &block)
       end
     end
     EOD
