865b45b6919d69e092748c8cad5e9670a7254c09
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2007-08-28 02:37:24 +0000

    Yay! Start first real regression test. Here we are just trying out some set/show commands.
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@294 453b2852-3d18-0410-866e-d09c099698e4

---
+++ test/cmdparse.cmd
@@ -0,0 +1,35 @@
+# $Id: cmdparse.cmd,v 1.20 2007/01/25 10:19:15 rockyb Exp $
+# This tests the functioning of some debugger command a
+# parsing and set/show processing
+set basename on
+### *******************************
+### ***   Set/show commands     ***
+### *******************************
+########################################
+###   test args and baseneme...
+########################################
+set args this is a test
+show args
+show basename
+set basename foo
+show base
+set basename off
+show basename
+set basename 0
+show basename
+set basename 1
+show basename
+########################################
+###   test listsize tests...
+########################################
+show listsize
+show listsi
+set listsize abc
+set listsize -20
+########################################
+###  test linetrace...
+########################################
+set linetrace on
+show linetrace
+set linetrace off
+show linetrace
