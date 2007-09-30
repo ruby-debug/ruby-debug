e64b96c0b4faa759caaa975c9fe5a33a09a021f3
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2007-09-30 13:07:10 +0000

    We're not using Diff::LCS so comment it out.
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@307 453b2852-3d18-0410-866e-d09c099698e4

--- test/test-stepping.rb
+++ test/test-stepping.rb
@@ -2,8 +2,8 @@
 require "test/unit"
 require "fileutils"
 require "rubygems"
-require "diff/lcs"
-require "diff/lcs/hunk"
+# require "diff/lcs"
+# require "diff/lcs/hunk"
 
 # require "ruby-debug"
 # Debugger.start
@@ -52,38 +52,38 @@ def cheap_diff(got_lines, correct_lines)
 end
 
 # Adapted from the Ruby Cookbook, Section 6.10: Comparing two files.
-def diff_as_string(rightfile, checkfile, format=:unified, context_lines=3)
-  right_data = File.read(rightfile)
-  check_data = File.read(checkfile)
-  output = ""
-  diffs = Diff::LCS.diff(right_data, check_data)
-  return output if diffs.empty?
-  oldhunk = hunk = nil
-  debugger
-  file_length_difference = 0
-  diffs.each do |piece|
-    begin
-      hunk = Diff::LCS::Hunk.new(right_data, check_data, piece, context_lines,
-                                 file_length_difference)
-      next unless oldhunk
+# def diff_as_string(rightfile, checkfile, format=:unified, context_lines=3)
+#   right_data = File.read(rightfile)
+#   check_data = File.read(checkfile)
+#   output = ""
+#   diffs = Diff::LCS.diff(right_data, check_data)
+#   return output if diffs.empty?
+#   oldhunk = hunk = nil
+#   debugger
+#   file_length_difference = 0
+#   diffs.each do |piece|
+#     begin
+#       hunk = Diff::LCS::Hunk.new(right_data, check_data, piece, context_lines,
+#                                  file_length_difference)
+#       next unless oldhunk
 
-      # Hunks may overlap, which is why we need to be careful when our
-      # diff includes lines of context. Otherwise, we might print
-      # redundant lines.
-      if (context_lines > 0) and hunk.overlaps?(oldhunk)
-        hunk.unshift(oldhunk)
-        else
-        output << oldhunk.diff(format)
-      end
-    ensure
-      oldhunk = hunk
-      output << "\n"
-    end
-  end
+#       # Hunks may overlap, which is why we need to be careful when our
+#       # diff includes lines of context. Otherwise, we might print
+#       # redundant lines.
+#       if (context_lines > 0) and hunk.overlaps?(oldhunk)
+#         hunk.unshift(oldhunk)
+#         else
+#         output << oldhunk.diff(format)
+#       end
+#     ensure
+#       oldhunk = hunk
+#       output << "\n"
+#     end
+#   end
   
-  # Handle the last remaining hunk 
-  output << oldhunk.diff(format) << "\n"
-end
+#   # Handle the last remaining hunk 
+#   output << oldhunk.diff(format) << "\n"
+# end
 
 # Test that linetracing does something.
 class TestStepping < Test::Unit::TestCase
