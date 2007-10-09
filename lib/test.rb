b60978bee66a1d2f348e6fde598f6ec745d6c082
superchris <superchris@81809f3d-796b-46d8-ad85-64d9ccf65f30>
2007-10-09 03:57:08 +0000

    very simple testcase
    git-svn-id: svn://rubyforge.org/var/svn/debug-commons/jruby-debug/trunk@293 81809f3d-796b-46d8-ad85-64d9ccf65f30

--- lib/test.rb
+++ lib/test.rb
@@ -1,6 +1,12 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+require 'test/unit'
 require 'ruby_debug_base.jar'
 
-Debugger.start
-puts "hi"
-Debugger.stop
+class DebuggerTest < Test::Unit::TestCase
 
+  def test_that_module_is_found
+    Debugger.start
+    Debugger.stop
+  end 
+
+end
