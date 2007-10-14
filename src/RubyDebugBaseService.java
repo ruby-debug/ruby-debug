1b1054e8fda37a3363bb762a9ee987c90fce32c8
mkrauskopf <mkrauskopf@81809f3d-796b-46d8-ad85-64d9ccf65f30>
2007-10-14 18:37:47 +0000

    trivial cleanup
    git-svn-id: svn://rubyforge.org/var/svn/debug-commons/jruby-debug/trunk@304 81809f3d-796b-46d8-ad85-64d9ccf65f30

--- src/RubyDebugBaseService.java
+++ src/RubyDebugBaseService.java
@@ -1,21 +1,11 @@
-
-
 import java.io.IOException;
 import org.jruby.Ruby;
-import org.jruby.RubyClass;
-import org.jruby.RubyThread;
 import org.jruby.debug.DebuggerDef;
-import org.jruby.runtime.Block;
-import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.runtime.load.BasicLibraryService;
-import org.jruby.runtime.load.Library;
 
 public final class RubyDebugBaseService implements BasicLibraryService {
 
     public boolean basicLoad(Ruby runtime) throws IOException {
-//        System.out.println("MK> " + new Exception().getStackTrace()[0] + " called...." + ", " + System.currentTimeMillis());
-//        java.util.logging.Logger.getLogger("MK>").info(" " + new Exception().getStackTrace()[0] + " called...." + ", " + System.currentTimeMillis());
-//        java.util.logging.Logger.getLogger("MK>").info("   HIT_COND_NONE: \"" + HIT_COND_NONE + "\"");
         DebuggerDef.createDebuggerModule(runtime);
         return true;
     }
