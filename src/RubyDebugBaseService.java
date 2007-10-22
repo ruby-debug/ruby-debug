caf9aaafb1710333293c11459d52c3a11a50054a
pbrant <pbrant@81809f3d-796b-46d8-ad85-64d9ccf65f30>
2007-10-22 01:53:12 +0000

    Rename DebuggerDef to RubyDebugger for more consistency with core (missed in previous commit)
    git-svn-id: svn://rubyforge.org/var/svn/debug-commons/jruby-debug/trunk@315 81809f3d-796b-46d8-ad85-64d9ccf65f30

--- src/RubyDebugBaseService.java
+++ src/RubyDebugBaseService.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
 import org.jruby.Ruby;
-import org.jruby.debug.DebuggerDef;
+import org.jruby.debug.RubyDebugger;
 import org.jruby.runtime.load.BasicLibraryService;
 
 public final class RubyDebugBaseService implements BasicLibraryService {
 
     public boolean basicLoad(Ruby runtime) throws IOException {
-        DebuggerDef.createDebuggerModule(runtime);
+        RubyDebugger.createDebuggerModule(runtime);
         return true;
     }
     
