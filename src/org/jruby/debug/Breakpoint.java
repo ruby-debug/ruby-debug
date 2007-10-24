dc283a876e31e44141ee8e947d4a0c804d1ddd57
pbrant <pbrant@81809f3d-796b-46d8-ad85-64d9ccf65f30>
2007-10-24 12:56:29 +0000

    Finish Ruby side of breakpoint implementation
    git-svn-id: svn://rubyforge.org/var/svn/debug-commons/jruby-debug/trunk@319 81809f3d-796b-46d8-ad85-64d9ccf65f30

--- src/org/jruby/debug/Breakpoint.java
+++ src/org/jruby/debug/Breakpoint.java
@@ -3,6 +3,7 @@ package org.jruby.debug;
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.RubyFixnum;
+import org.jruby.RubyNumeric;
 import org.jruby.RubyObject;
 import org.jruby.anno.JRubyMethod;
 import org.jruby.runtime.Block;
@@ -29,17 +30,29 @@ public class Breakpoint extends RubyObject {
 
     @JRubyMethod(name="source=", required=1)
     public IRubyObject source_set(IRubyObject source, Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        debuggerBreakpoint().setSource(source.convertToString());
+        
+        return source;
     }
 
     @JRubyMethod(name="pos")
     public IRubyObject pos(Block block) {
-        return getRuntime().newFixnum(debuggerBreakpoint().getPos().getLine());
+        DebugBreakpoint debugBreakpoint = debuggerBreakpoint();
+        if (debugBreakpoint.getType() == DebugBreakpoint.Type.METHOD) {
+            return getRuntime().newString(debuggerBreakpoint().getPos().getMethodName());
+        } else {
+            return getRuntime().newFixnum(debuggerBreakpoint().getPos().getLine());    
+        }
     }
 
     @JRubyMethod(name="pos=", required=1)
     public IRubyObject pos_set(IRubyObject pos, Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        DebugBreakpoint debugBreakpoint = debuggerBreakpoint();
+        if (debugBreakpoint.getType() == DebugBreakpoint.Type.METHOD) {
+            return getRuntime().newString(debuggerBreakpoint().getPos().getMethodName());
+        } else {
+            return getRuntime().newFixnum(debuggerBreakpoint().getPos().getLine());    
+        }
     }
 
     @JRubyMethod(name="expr")
@@ -49,7 +62,9 @@ public class Breakpoint extends RubyObject {
 
     @JRubyMethod(name="expr=", required=1)
     public IRubyObject expr_set(IRubyObject expr, Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        debuggerBreakpoint().setExpr(expr.convertToString());
+        
+        return expr;
     }
 
     @JRubyMethod(name="hit_count")
@@ -59,21 +74,55 @@ public class Breakpoint extends RubyObject {
 
     @JRubyMethod(name="hit_value")
     public IRubyObject hit_value(Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        return getRuntime().newFixnum(debuggerBreakpoint().getHitValue());
     }
 
     @JRubyMethod(name="hit_value=", required=1)
     public IRubyObject hit_value_set(IRubyObject hit_value, Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        debuggerBreakpoint().setHitValue(RubyNumeric.fix2int(hit_value));
+        
+        return hit_value;
     }
 
     @JRubyMethod(name="hit_condition")
     public IRubyObject hit_condition(Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        DebugBreakpoint.HitCondition cond = debuggerBreakpoint().getHitCondition();
+        if (cond == null) {
+            return getRuntime().getNil();
+        } else {
+            switch (cond) {
+            case GE:
+                return getRuntime().newSymbol("greater_or_equal");
+            case EQ:
+                return getRuntime().newSymbol("equal");
+            case MOD:
+                return getRuntime().newSymbol("modulo");
+            case NONE:
+            default:
+                return getRuntime().getNil();
+            }
+        }
     }
 
     @JRubyMethod(name="hit_condition=", required=1)
     public IRubyObject hit_condition_set(IRubyObject hit_condition, Block block) {
-        throw new UnsupportedOperationException("not implemented yet");
+        DebugBreakpoint debugBreakpoint = debuggerBreakpoint();
+        
+        if (! hit_condition.isKindOf(getRuntime().getSymbol())) {
+            throw getRuntime().newArgumentError("Invalid condition parameter");
+        }
+        
+        String symbol = hit_condition.asSymbol();
+        if (symbol.equals("greater_or_equal") || symbol.equals("ge")) {
+            debugBreakpoint.setHitCondition(DebugBreakpoint.HitCondition.GE);
+        } else if (symbol.equals("equal") || symbol.equals("eq")) {
+            debugBreakpoint.setHitCondition(DebugBreakpoint.HitCondition.EQ);
+        } else if (symbol.equals("modulo") || symbol.equals("mod")) {
+            debugBreakpoint.setHitCondition(DebugBreakpoint.HitCondition.MOD);
+        } else {
+            throw getRuntime().newArgumentError("Invalid condition parameter");
+        }
+        
+        return hit_condition;
     }
 }
