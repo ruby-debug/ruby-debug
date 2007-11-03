af623fb5b8ba7ff0cd5eeb0fb738403d1df9931b
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2007-11-03 21:20:16 +0000

    Move to the right place (under emacs)
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@321 453b2852-3d18-0410-866e-d09c099698e4

--- emacs/rdebug-test.el
+++ emacs/rdebug-test.el
@@ -3,22 +3,22 @@
 (load-file "./rdebug-extra.el")
 (load-file "./rdebug-track.el")
 
-(defun regexp-test (location-str file-str)
+(defun regexp-file-test (location-str file-str)
   "Test to see that location-str matches gud-rubydb-marker-regexp"
-  (assert-equal 0 (string-match gud-rubydb-marker-regexp location-str))
+  (assert-equal 0 (string-match gud-rdebug-marker-regexp location-str))
   (assert-equal file-str
 		(substring location-str (match-beginning 1)  (match-end 1)))
 )
-(deftest "rdebug-marker-regexp-test"
+(deftest "rdebug-regexp-file-test"
 
-  (regexp-test 
-   "\032\032./hanoi.rb:3:\n"
+  (regexp-file-test 
+   "\032\032./hanoi.rb:3\n"
    "./hanoi.rb"
    )
 )
 
-(defun position-regexp-test (location-str file-str line-str)
-  "Test to see that location-str matches position-regexp-test with the correct
+(defun regexp-position-test (location-str file-str line-str)
+  "Test to see that location-str matches position-regexp-file-test with the correct
 file and line submatches."
   (assert-equal 0 (string-match rdebug-position-re location-str))
   (assert-equal file-str (match-string rdebug-marker-regexp-file-group
@@ -26,15 +26,30 @@ file and line submatches."
   (assert-equal line-str (match-string rdebug-marker-regexp-line-group
                                        location-str))
   )
-(deftest "rdebug-position-re-test"
 
-  (position-regexp-test 
+(deftest "rdebug-regexp-position-test"
+
+  (position-regexp-file-test 
    "\032\032./hanoi.rb:3:\n"
    "./hanoi.rb" "3"
    )
 )  
    
-(build-suite "rdebug-suite" "rdebug-marker-regexp-test" "rdebug-position-re-test")
+(deftest "rdebug-get-script-name-test"
+  (assert-equal '("foo" nil) (rdebug-get-script-name '("foo")))
+  (assert-equal '("foo" nil) (rdebug-get-script-name '("-m" "foo")))
+  (assert-equal '("foo" t) (rdebug-get-script-name '("--annotate=1" "foo")))
+  (assert-equal '("foo" t) (rdebug-get-script-name '("--annotate" "1" "foo")))
+  (assert-equal '("foo" t) (rdebug-get-script-name '("-A" "1" "foo")))
+  (assert-equal '("foo" nil) 
+		(rdebug-get-script-name 
+		 '("rdebug" "--include" "me" "-n" "foo")))
+  (assert-equal '("foo" nil) (rdebug-get-script-name 
+			      '("rdebug" "--server" "-d" "--host"
+				"localhost" "foo" "-1")))
+)
+
+(build-suite "rdebug-suite" "rdebug-regexp-file-test" "rdebug-regexp-position-test")
 (run-elk-test "rdebug-suite"
               "test regular expression used in tracking lines")
 
