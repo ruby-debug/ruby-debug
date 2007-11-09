9c7a37639d1d7552d150b7e6496d6aff107875ea
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2007-11-09 18:27:43 +0000

    Add "method iv" (instance_variables)
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@337 453b2852-3d18-0410-866e-d09c099698e4

--- cli/ruby-debug/commands/method.rb
+++ cli/ruby-debug/commands/method.rb
@@ -1,11 +1,16 @@
 module Debugger
   class MethodCommand < Command # :nodoc:
     def regexp
-      /^\s*m(?:ethod)?\s+(?:(i(:?nstance)?)\s)?/
+      /^\s*m(?:ethod)?\s+((iv)|(i(:?nstance\s+)?)\s+)?/
     end
 
     def execute
-      if @match[1]
+      if @match[1] == "iv"
+        obj = debug_eval(@match.post_match)
+        obj.instance_variables.each do |v|
+          print "%s = %s\n", v, obj.instance_variable_get(v)
+        end
+      elsif @match[1]
         obj = debug_eval(@match.post_match)
         print "%s\n", columnize(obj.methods.sort(), 
                                 self.class.settings[:width])
@@ -28,6 +33,7 @@ module Debugger
       def help(cmd)
         %{
           m[ethod] i[nstance] <obj>\tshow methods of object
+          m[ethod] iv <obj>\tshow instance variables of object
           m[ethod] <class|module>\t\tshow instance methods of class or module
         }
       end
