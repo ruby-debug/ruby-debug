0e4a20ca9bfadde48713b7f0710de2abc5a43e50
andersl <andersl@453b2852-3d18-0410-866e-d09c099698e4>
2007-12-29 09:29:06 +0000

    When the end of the program is reached and the --no-quit option is used, the user now steps into the file 'theend.rb'. This file contains a message telling the user that the end of the application has been reached. For a normal debug session this feels better. However, if the user file is empty, this prevents the debugger from going into an infinite loop.
    Note, an alternative implementation would be if the debugger itself
    would create an artificial step point at the of the the user program.
    
    
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@505 453b2852-3d18-0410-866e-d09c099698e4

---
+++ cli/ruby-debug/theend.rb
@@ -0,0 +1,13 @@
+######################################################################
+##                                                                ####
+##                          T H E    E N D                        ####
+##                                                                ####
+######################################################################
+
+#
+# You have reached the end of the debugged program.
+#
+# The next ruby debugger step or run command will restart the program.
+#
+
+nil
