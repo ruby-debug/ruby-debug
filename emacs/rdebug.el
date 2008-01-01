dd5f698ddd2142bc6d6d8f32ce33e903e4169fdc
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2008-01-01 08:18:13 +0000

    Add rdebug-user-separate-io-buffer to let folks decide where output should appear - similar to gdb-ui's rdebug-user-separate-io-buffer. Move customizable variables from rdebug-core.el to rdebug.el.
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@526 453b2852-3d18-0410-866e-d09c099698e4

--- emacs/rdebug.el
+++ emacs/rdebug.el
@@ -1,7 +1,7 @@
 ;;; rdebug.el --- Ruby debugger user interface, startup file.
 
-;; Copyright (C) 2006, 2007 Rocky Bernstein (rocky@gnu.org)
-;; Copyright (C) 2007 Anders Lindgren
+;; Copyright (C) 2006, 2007, 2008 Rocky Bernstein (rocky@gnu.org)
+;; Copyright (C) 2007, 2008 Anders Lindgren
 
 ;; $Id: rdebug.el 409 2007-12-14 02:36:37Z rockyb $
 
@@ -92,6 +92,109 @@ This function is designed to be added to hooks, for example:
 
 (add-hook 'ruby-mode-hook 'rdebug-turn-on-debugger-support)
 
+
+;; -------------------------------------------------------------------
+;; user definable variables
+;;
+
+(defcustom gud-rdebug-command-name
+  "rdebug --emacs"
+  "File name for executing the Ruby debugger and command options.
+This should be an executable on your path, or an absolute file name."
+  :type 'string
+  :group 'gud)
+
+(defcustom rdebug-line-width 120
+  "Length of line before truncation occurs.
+This value limits output in secondary buffers."
+  :type 'integer
+  :group 'rdebug)
+
+(defcustom rdebug-many-windows t
+  "*If non-nil, use the full debugger user interface, see `rdebug'.
+
+However only set to the multi-window display if the rdebug
+command invocation has an annotate options (\"--annotate 3\")."
+  :type 'boolean
+  :group 'rdebug)
+
+(defcustom rdebug-populate-common-keys-function
+  'rdebug-populate-common-keys-standard
+  "The function to call to populate key bindings common to all rdebug windows.
+This includes the secondary windows, the debugger shell, and all
+Ruby source buffers when the debugger is active.
+
+This variable can be bound to the following:
+
+* nil -- Don't bind any keys.
+
+* `rdebug-populate-common-keys-standard' -- Bind according to a widely used
+  debugger convention:
+
+\\{rdebug-example-map-standard}
+
+* `rdebug-populate-common-keys-eclipse' -- Bind according to Eclipse.
+
+\\{rdebug-example-map-eclipse}
+
+* `rdebug-populate-common-keys-netbeans' -- Bind according to NetBeans.
+
+\\{rdebug-example-map-netbeans}
+
+* Any other value is expected to be a callable function that takes one
+  argument, the keymap, and populates it with suitable keys."
+  :type 'function
+  :group 'rdebug)
+
+(defcustom rdebug-restore-original-window-configuration :many
+  "*Control if the original window layout is restored when the debugger exits.
+The value can be t, nil, or :many.
+
+A value of t means that the original layout is always restored,
+nil means that it's never restored.
+
+:many means that the original layout is restored only when
+`rdebug-many-windows' is used."
+  :type '(choice (const :tag "Always restore" t)
+		 (const :tag "Never restore" nil)
+		 (const :tag "Restore in many windows mode" :many))
+  :group 'rdebug)
+
+(defcustom rdebug-use-separate-io-buffer nil
+  "Non-nil means display output from the debugged program in a separate buffer."
+  :type 'boolean
+  :group 'gud)
+
+
+(defcustom rdebug-window-layout-function
+  'rdebug-window-layout-standard
+  "*A function that performs the window layout of `rdebug'.
+
+This is only used in `rdebug-many-windows' mode. This should be
+bound to a function that performs the actual window layout. The
+function should takes two arguments, the first is the source
+buffer and the second the name of the script to debug.
+
+Rdebug provides the following predefined layout functions:
+
+* `rdebug-window-layout-standard'         -- See `rdebug'
+
+* `rdebug-window-layout-conservative'     -- Source + Shell + Output
+
+* `rdebug-window-layout-stack-of-windows' -- Extra windows to the right
+
+* `rdebug-window-layout-rocky'            -- Rocky's own layout"
+  :type
+  '(choice
+    (function :tag "Standard"         rdebug-window-layout-standard)
+    (function :tag "Conservative"     rdebug-window-layout-conservative)
+    (function :tag "Stack of windows" rdebug-window-layout-stack-of-windows)
+    (function :tag "Rocky's own"      rdebug-window-layout-rocky)
+    (function :tag "Other"            function))
+  :group 'rdebug)
+
+
+
 (provide 'rdebug)
 
 ;;; rdebug.el ends here
