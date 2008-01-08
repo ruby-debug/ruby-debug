cbc301edbd3907184b75eb36a9f80db134be994e
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2008-01-08 16:04:18 +0000

    Restore $: to the value it was before rdebug call.
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@570 453b2852-3d18-0410-866e-d09c099698e4

--- rdbg.rb
+++ rdbg.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
+# $Id$
 
 # Use this to run rdebug without installing it.  We assume that the
 # library directories are stored at the same level the directory
 # this program. 
 module RDebugRunner
   def runner(stdout=nil)
+
+    # Add libraries to load path.
     dirname=File.dirname(__FILE__)
-    libs=%w(ext lib cli).map {|lib| File.join(dirname, lib) }
-    # old_search=$:
-    $:.unshift(*libs)
+    libs = %w(ext lib cli)
+    libs.each { |lib| $:.unshift File.join(dirname, lib) }
+
     rdebug=ENV['RDEBUG'] || File.join(dirname, 'bin', 'rdebug')
     if stdout
       old_stdout = $stdout
@@ -16,14 +19,15 @@ module RDebugRunner
     else
       old_stdout = nil
     end
-    load(rdebug)
+    load(rdebug, true)
     $stdout.reopen(old_stdout) if old_stdout
-    # $: = old_search
+    
+    # Remove those libraries we just added.
+    1.upto(libs.size) {$:.shift}
   end
   module_function :runner
 end
 if __FILE__ == $0 or
     ($DEBUG and ['rcov', 'rdebug'].include?(File.basename($0)))
-  include RDebugRunner
-  runner
+  RDebugRunner.runner
 end
