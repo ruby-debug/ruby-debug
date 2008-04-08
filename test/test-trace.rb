97c6a62ecd43e446caa5d1da6a3a7d97f0a8efe4
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2008-04-08 10:23:15 +0000

    Another linetrace test. This time with linetrace+ set.
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@801 453b2852-3d18-0410-866e-d09c099698e4

--- test/test-trace.rb
+++ test/test-trace.rb
@@ -43,4 +43,21 @@ class TestEdit < Test::Unit::TestCase
 
     end
   end
+
+  def test_linetrace_plus_command
+
+    filter = Proc.new{|got_lines, correct_lines|
+        got_lines.collect!{|l| l !~ /:rdbg\.rb:/? l : nil}.compact!
+      }
+
+    testname='linetracep'
+    Dir.chdir(@@SRC_DIR) do 
+      script = File.join('data', testname + '.cmd')
+      assert_equal(true, 
+                   run_debugger(testname,
+                                "--script #{script} -- gcd.rb 3 5", nil, 
+                                filter))
+
+    end
+  end
 end
