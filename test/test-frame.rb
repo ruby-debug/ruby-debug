c54a42bcf76c04336cd8d8ad6d4a98248314bb89
rockyb <rockyb@453b2852-3d18-0410-866e-d09c099698e4>
2008-05-05 18:05:14 +0000

    make test-frame installation independent. Bug #19931
    git-svn-id: svn://rubyforge.org/var/svn/ruby-debug/trunk@831 453b2852-3d18-0410-866e-d09c099698e4

--- test/test-frame.rb
+++ test/test-frame.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require 'test/unit'
 
-# begin require 'rubygems' rescue LoadError end
-# require 'ruby-debug'; Debugger.start
+# require 'rubygems'
+# require 'ruby-debug'; Debugger.start(:post_mortem => true)
 
 # Test frame commands
 class TestFrame < Test::Unit::TestCase
@@ -16,11 +16,19 @@ class TestFrame < Test::Unit::TestCase
   # Test commands in frame.rb
   def test_basic
     testname='frame'
+    # Ruby 1.8.6 and earlier have a trace-line number bug for return
+    # statements.
+    filter = Proc.new{|got_lines, correct_lines|
+      [got_lines[11], got_lines[11]].flatten.each do |s|
+        s.sub!(/in file ".*gcd.rb/, 'in file "gcd.rb')
+      end
+    }
     Dir.chdir(@@SRC_DIR) do 
       script = File.join('data', testname + '.cmd')
       assert_equal(true, 
                    run_debugger(testname,
-                                "--script #{script} -- gcd.rb 3 5"))
+                                "--script #{script} -- gcd.rb 3 5",
+                                nil, filter))
     end
   end
 end
