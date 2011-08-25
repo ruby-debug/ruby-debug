cf3bc69c1bb9f5e5a6b3d0c41f1abd7511b35675
John Firebaugh <john_firebaugh@us.ibm.com>
2011-08-25 11:41:33 -0700

    Make sure test-quit.rb actually tests the quit command

--- test/test-quit.rb
+++ test/test-quit.rb
@@ -8,11 +8,6 @@ class TestQuit < Test::Unit::TestCase
   def test_basic
     testname='quit'
     script = File.join('data', testname + '.cmd')
-#       filter = Proc.new{|got_lines, correct_lines|
-#         [got_lines[0], correct_lines[0]].each do |s|
-#           s.sub!(/tdebug.rb:\d+/, 'rdebug:999')
-#         end
-#       }
-    assert(run_debugger(testname, "--script #{script} -- ./example/null.rb"))
+    assert(run_debugger(testname, "--script #{script} -- ./example/gcd.rb"))
   end
 end
